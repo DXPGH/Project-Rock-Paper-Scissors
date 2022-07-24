c8a9314a64fc79601ff6e644ec27160acf761b3c
DXPGH <dxpbusiness@gmail.com>
2022-07-24 14:58:23 -0500

    Added function for 5 round games

--- app.js
+++ app.js
@@ -1,50 +1,69 @@
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-let playerSelection = getPlayerSelection();
-console.log(playerSelection);
+let computerSelection;
+let playerSelection;
+let playerNum;
+let computerNum;
 
-// Randomly return either Rock Paper or Scissors
+// Randomly return either Rock Paper or Scissors for Computer input
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
-      return "ROCK";
+      return (computerSelection = "ROCK");
     case 1:
-      return "PAPER";
+      return (computerSelection = "PAPER");
     case 2:
-      return "SCISSORS";
+      return (computerSelection = "SCISSORS");
   }
 }
 
+//Collecting Player input
 function getPlayerSelection() {
   let userInput = prompt("Rock, Paper, Scissors?");
-  return userInput.toUpperCase();
+  return (playerSelection = userInput.toUpperCase());
 }
 
-function playRound(playerSelection, computerSelection) {
+//Play single RPS Round
+function playRound() {
+  getComputerChoice();
+  getPlayerSelection();
 
   if (playerSelection === computerSelection) {
     tie();
-    console.log(playerSelection);
-    console.log(computerSelection);
   }
 
   if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "ROCK")) {
     win();
+    playerNum++;
   }
   if ((computerSelection === "ROCK" && playerSelection === "SCISSORS") || (computerSelection === "SCISSORS" && playerSelection === "PAPER") || (computerSelection === "PAPER" && playerSelection === "ROCK")) {
     lose();
+    computerNum++;
   }
 }
 
+//Play a 5 round game of RPS
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    if (playerNum === computerNum) {
+      document.getElementById("overall-results").innerHTML = "<br>YOU TIED LAME!";
+    } else if (playerNum > computerNum) {
+      document.getElementById("overall-results").innerHTML = "<br> YOU WON~!";
+    } else {
+      document.getElementById("overall-results").innerHTML = "<br>YOU LOST! D:";
+    }
+  }
+}
+
+//Resulting outcome of each round
 function tie() {
-    console.log("Tie");
+  document.getElementById("results").innerHTML += "Tie! <br>";
 }
 
 function win() {
-    console.log("You Win!!");
+  document.getElementById("results").innerHTML += "You Win! <br>";
 }
 
 function lose() {
-    console.log("You Lost! D:");
+  document.getElementById("results").innerHTML += "You Lost! <br>";
 }

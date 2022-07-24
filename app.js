43f663d9cd707d951d342ddc666cb70234431a71
DXPGH <dxpbusiness@gmail.com>
2022-07-24 14:38:03 -0500

    Implemented rock paper scissors play one round

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+let playerSelection = getPlayerSelection();
+console.log(playerSelection);
 
+// Randomly return either Rock Paper or Scissors
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
+}
+
+function getPlayerSelection() {
+  let userInput = prompt("Rock, Paper, Scissors?");
+  return userInput.toUpperCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection === computerSelection) {
+    tie();
+    console.log(playerSelection);
+    console.log(computerSelection);
+  }
+
+  if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "ROCK")) {
+    win();
+  }
+  if ((computerSelection === "ROCK" && playerSelection === "SCISSORS") || (computerSelection === "SCISSORS" && playerSelection === "PAPER") || (computerSelection === "PAPER" && playerSelection === "ROCK")) {
+    lose();
+  }
+}
+
+function tie() {
+    console.log("Tie");
+}
+
+function win() {
+    console.log("You Win!!");
+}
+
+function lose() {
+    console.log("You Lost! D:");
+}
